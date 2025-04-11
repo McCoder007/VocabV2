c093beac7b1d345e811584d4cc5f6db76ece0bb7
McCoder007 <shawn@mcgrathplace.com>
2025-04-11 05:38:50 +0000

    Deploying to gh-pages from @ McCoder007/VocabV2@732f68e02018a190ecf9740c1a4c090ca26fcd58 🚀

--- tts-manager.js
+++ tts-manager.js
@@ -12,7 +12,8 @@ class GoogleTTSManager {
     }
 
     // This will be replaced during build
-    static API_KEY = 'AIzaSyBBe1XfNjodUza5EHDLbs6HTWk8O64b5c8';
+    static API_KEY = 'AIzaSyBBe1XfNjodUza5EHDLbs6HTWk8O64b5c8
+';
 
     /**
      * Speak the given text using Google TTS API with browser fallback
